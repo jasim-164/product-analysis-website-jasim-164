49c9354673cd3064604ca891d871d3d2e3170bd1
jasim-164 <jasimuddincse1703164@gmail.com>
2022-04-04 16:24:47 +0600

    404 page create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
+
+import { Route, Routes,Link } from 'react-router-dom';
+import Home from './components/Home/Home'
+import Navbar from './components/Navbar/Navbar'
+import NotFound from './components/NotFound/NotFound'
 import './App.css';
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>ami boro papi bandha</h1>
+        </div>
+    );
+};
+
+export default Navbar;
